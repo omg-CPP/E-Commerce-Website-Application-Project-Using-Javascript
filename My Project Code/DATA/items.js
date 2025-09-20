8ff81e570c469f18d7874b024d67a42740218506
Om Gupta <145378690+omg-CPP@users.noreply.github.com>
2025-09-20 12:55:16 +0530

    Add files via upload

---
+++ My Project Code/DATA/items.js
@@ -0,0 +1,147 @@
+// array of objects representing items
+const items = [
+    {
+        id: '001',
+        item_image: 'images/1.jpg',
+        rating: {
+            stars: 4.5,
+            noOfReviews: 1400
+        },
+        company_name: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        price: {
+            current_price: 606,
+            original_price: 1045,
+            discount: 42
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '002',
+        item_image: 'images/2.jpg',
+        rating: {
+            stars: 4.3,
+            noOfReviews: 2400
+        },
+        company_name: 'CUKOO',
+        item_name: 'Women Padded Halter Neck Swimming Dress',
+        price: {
+            current_price: 1507,
+            original_price: 2599,
+            discount: 42
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '003',
+        item_image: 'images/3.jpg',
+        rating: {
+            stars: 4.1,
+            noOfReviews: 249
+        },
+        company_name: 'NUEVOSDAMAS',
+        item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
+        price: {
+            current_price: 495,
+            original_price: 1599,
+            discount: 69
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '004',
+        item_image: 'images/4.jpg',
+        rating: {
+            stars: 5.0,
+            noOfReviews: 10
+        },
+        company_name: 'ADIDAS',
+        item_name: 'Indian Cricket ODI Jersey',
+        price: {
+            current_price: 999,
+            original_price: 999,
+            discount: 0
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '005',
+        item_image: 'images/5.jpg',
+        rating: {
+            stars: 4.2,
+            noOfReviews: 3500
+        },
+        company_name: 'Roadster',
+        item_name: 'Pure Cotton T-shirt',
+        price: {
+            current_price: 489,
+            original_price: 1399,
+            discount: 65
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '006',
+        item_image: 'images/6.jpg',
+        rating: {
+            stars: 0.0,
+            noOfReviews: 0
+        },
+        company_name: 'Nike',
+        item_name: 'Men ReactX Running Shoes',
+        price: {
+            current_price: 14995,
+            original_price: 14995,
+            discount: 0
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '007',
+        item_image: 'images/7.jpg',
+        rating: {
+            stars: 4.2,
+            noOfReviews: 388
+        },
+        company_name: 'The Indian Garage Co',
+        item_name: 'Men Slim Fit Regular Shorts',
+        price: {
+            current_price: 639,
+            original_price: 1599,
+            discount: 60
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    },
+
+    {
+        id: '008',
+        item_image: 'images/8.jpg',
+        rating: {
+            stars: 4.2,
+            noOfReviews: 5200
+        },
+        company_name: 'Nivea',
+        item_name: 'Men Fresh Deodrant 150ml',
+        price: {
+            current_price: 142,
+            original_price: 285,
+            discount: 50
+        },
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+    }
+];
+
